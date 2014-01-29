595759058eda1a0816a69b344266aa58d710a2cc
Isa Herico <miss.herico@gmail.com>
2014-01-29 09:58:50 -0800

    front end fun.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -14,6 +14,10 @@ class CategoriesController < ApplicationController
   end
 
   def edit
+  	
+  end
+
+  def update
   end
 
   def destroy
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,8 +10,9 @@ class ProductsController < ApplicationController
 
   def show
   	id = params[:id]
-  	@product = Product.find_by(id)
+  	@product = Product.find(id)
   	@categories = @product.categories
+
   end
 
   def edit
@@ -19,9 +20,49 @@ class ProductsController < ApplicationController
   	@product = Product.find_by(id)
   	@categories = @product.categories
 
+  	@all_categories = Category.all
+
   end
 
   def update
+  	#binding.pry
+  	id = params[:id]
+  	product = Product.find(id)
+    user_input = params.require(:product).permit(:name, :description, :price)
+  
+    product.update_attributes(user_input)
+
+    category_ids = params[:product][:category_ids].select {|c| c != ""}
+
+   
+
+    cats = category_ids.map do |id|
+    	Category.find(id)
+    end
+   raise cats.inspect
+    product.categories = cats
+
+
+    # product.category_ids.each do |id|
+    # 	unless category_ids.include?(id)
+    # 		product.categories.delete(id)
+    # 		category_ids.delete(id)
+    # 	end
+    # end
+
+    # prod_ids = product.category_ids
+
+
+    # category_ids.each do |cat_id|
+    # 	unless prod_ids.include?(cat_id.to_i)
+    # 		product.categories << Category.find(cat_id)
+    # 	end
+    # end
+
+
+    # binding.pry
+    #raise params.inspect
+    redirect_to product_path(product)
   end
 
   def destroy
